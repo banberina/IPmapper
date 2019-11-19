c78710577546fd58b04e5e091e15c217c1e1f0d0
Berina Bandić <44987552+banberina@users.noreply.github.com>
2019-11-19 10:21:22 +0100

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    MONGODB_URL: 'mongodb+srv://admin:admin@cluster0-vqejn.mongodb.net/ipmapper?retryWrites=true&w=majority',
-    CLIENT_ID: '740300235550-hqjjutfvd30t2bh7jlb8hbqpv8qgcr30.apps.googleusercontent.com',
-    CLIENT_SECRET: '5H_KA1c3YI2yOA-WzoQ4tV3c',
-    REDIRECT_URL: "http://localhost:4000/login",
-    JWT_SECRET: ''
-}
