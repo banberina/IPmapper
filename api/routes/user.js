4ba9d33439cd277786355ee74c92e29c1329ab7e
banberina <berina.bandic@gmail.com>
2019-12-02 09:53:44 +0100

    User route added.

---
+++ api/routes/user.js
@@ -0,0 +1,42 @@
+module.exports = (router, db, mongojs, jwt, config) => {
+    
+    router.use((req, res, next) => {
+        console.log(`User route accessed by: ${req.ip}` ); // log visits
+
+        /* Check for proper JWT */
+        let authorization = req.get('Authorization');
+        if (authorization) {
+            jwt.verify(authorization, process.env.JWT_SECRET || config.JWT_SECRET, (error, decoded) => {
+                if (error) {
+                    res.status(401).send({ message: 'Unauthorized access: ' + error.message });
+                } else {
+                    let userType = decoded.type;
+                    if (userType === 'user') {
+                        next();
+                    } else {
+                        res.status(401).send({ message: 'Unauthorized access: improper privileges'});
+                    }
+                }
+            });
+        } else {
+            res.status(401).send({ message: 'Unauthorized access.' });
+        }
+    })
+    
+    /* Visit-logging middleware */
+   router.use((req, res, next, mongojs, db,config) => {
+       console.log(`New visit from ${req.ip} at ${new Date()}` ); // log visits
+       next();
+   });
+
+   router.get('/',(req,res)=>{
+       let limit = Number(req.query.limit)||10;
+       let skip = Number(req.query.skip)||0;
+       db.geo.find({}).skip(skip).limit(limit,(error,docs)=> {
+           if(error) {
+               throw error;
+           }
+           res.json(docs);
+       });
+   }); 
+}
