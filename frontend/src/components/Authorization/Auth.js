c25f8536debbc4c32f38545a7cf1e9cefb8395e7
banberina <berina.bandic@gmail.com>
2020-01-14 17:23:07 +0100

    tweaks on auth.js

--- frontend/src/components/Authorization/Auth.js
+++ frontend/src/components/Authorization/Auth.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom';
 
 class Auth extends Component {
     constructor(props) {
@@ -9,9 +10,8 @@ class Auth extends Component {
     componentDidMount = () => {
         /* If a token is already set, redirect to (logged in) homepage */
         if (localStorage.getItem('jwtToken')) {
-            // this.props.history.push('/');
-            // window.location.reload();
-            window.location.replace("/")
+            this.props.history.push('/');
+            window.location.reload();
         } else {
             /* Otherwise, take the token from the URL, store it and then redirect to the appropriate page */
             let token = (new URL(document.location)).hash.split('#jwt=')[1];
@@ -19,9 +19,8 @@ class Auth extends Component {
                 this.props.history.push('/');
             } else {
                 localStorage.setItem('jwtToken', token);
-                // this.props.history.push('/');
-                // window.location.reload();
-                window.location.replace("/")
+                this.props.history.push('/');
+                window.location.reload();
             }
         }
     }
